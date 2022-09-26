4ea52a03d3cb2952fb460a68ec0e476f4b021b3a
diskostu <denny.kluge@gmail.com>
2022-09-26 20:43:11 +0200

    finished "events in Compose"

--- app/src/main/java/de/diskostu/android/basicstatecodelab/WaterCounter.kt
+++ app/src/main/java/de/diskostu/android/basicstatecodelab/WaterCounter.kt
@@ -1,6 +1,8 @@
 package de.diskostu.android.basicstatecodelab
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -9,11 +11,19 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-    val count = 0
-    Text(
-        text = "You've had $count glasses.",
-        modifier = modifier.padding(16.dp)
-    )
+    var count = 0
+
+    Column(modifier = modifier.padding(16.dp)) {
+        Text(
+            text = "You've had $count glasses."
+        )
+        Button(
+            onClick = { count++ },
+            modifier = modifier.padding(top = 8.dp)
+        ) {
+            Text(text = "One more glass")
+        }
+    }
 }
 
 
