08fc91cbf346b628fc4f39f0bea7a5543afff177
diskostu <denny.kluge@gmail.com>
2022-09-26 21:02:48 +0200

    finished "memory in a composable function"

--- app/src/main/java/de/diskostu/android/basicstatecodelab/WaterCounter.kt
+++ app/src/main/java/de/diskostu/android/basicstatecodelab/WaterCounter.kt
@@ -1,17 +1,19 @@
 package de.diskostu.android.basicstatecodelab
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
+@SuppressLint("UnrememberedMutableState")
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-    var count = 0
+    var count by remember { mutableStateOf(0) }
 
     Column(modifier = modifier.padding(16.dp)) {
         Text(
